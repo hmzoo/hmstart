53e78139ec092d1151727f5fcedff630896da0bf
hmj <hmj@goatpage.com>
2016-06-14 20:41:40 +0200

    watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,61 @@ var less = require('gulp-less');
 var rename = require('gulp-rename');
 var handlebars = require('gulp-compile-handlebars');
 var express = require('express');
+var browserSync = require('browser-sync');
+var gutil = require('gulp-util');
 
+var server;
 
-gulp.task('server',function(){
-var server=express();
-server.use(express.static('dist'));
-server.listen(3000)
+var reload = function() {
+    if (server) {
+        return browserSync.reload({
+            stream: true
+        });
+    }
+    return gutil.noop();
+
+}
+
+gulp.task('server', function() {
+    server = express();
+    server.use(express.static('dist'));
+    server.listen(3000);
+    browserSync({
+        proxy: 'localhost:3000'
+    });
 });
 
-gulp.task('less', function () {
-  return gulp.src('./src/styles/*.less')
-    .pipe(less())
-    .pipe(gulp.dest('./dist'));
+gulp.task('less', function() {
+    return gulp.src('./src/styles/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./dist'))
+        .pipe(reload());
+
 });
 
-gulp.task('templates', function () {
+gulp.task('templates', function() {
     var templateData = {
         title: 'HM start'
     };
-  
+
     var options = {
         ignorePartials: true,
-        partials : {},
-        batch : ['./src/templates/'],
-        helpers : {}
+        partials: {},
+        batch: ['./src/templates/'],
+        helpers: {}
     };
 
     return gulp.src('./src/templates/index.handlebars')
         .pipe(handlebars(templateData, options))
         .pipe(rename('index.html'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist'))
+        .pipe(reload());
 });
+gulp.task('watch', function() {
+    gulp.watch('./src/styles/*.less', ['less']);
+    gulp.watch('./src/templates/*.handlebars', ['templates']);
+    //gulp.watch('./src/templates/*.handlebars',['templates']);
+
+});
+gulp.task('build', ['templates', 'less']);
+gulp.task('default', ['build', 'watch', 'server'])
