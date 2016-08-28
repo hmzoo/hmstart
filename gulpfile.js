2b2c7a42da492f96d820f5164b27632bf88f6893
hmj <hmj@goatpage.com>
2016-08-28 22:17:17 +0200

    socket.io

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
-var less = require('gulp-less')
-var pug = require('gulp-pug')
+var less = require('gulp-less');
+var pug = require('gulp-pug');
+var concat = require('gulp-concat');
 
 
 gulp.task('less',function(){
@@ -15,4 +16,16 @@ gulp.task('pug',function(){
     .pipe(gulp.dest('dist/'))
 });
 
-gulp.task('build', [ 'less','pug']);
+gulp.task('server', function() {
+  return gulp.src(['app/server/server.js'])
+    .pipe(concat('server.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('client', function() {
+  return gulp.src(['node_modules/socket.io-client/socket.io.js','app/client/app.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./dist/public'));
+});
+
+gulp.task('build', [ 'server','client','less','pug']);
